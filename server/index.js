8c315d582637e4252d1c483d9af444048fb5240a
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-17 22:10:10 +0530

    added health check endpoint

--- server/index.js
+++ server/index.js
@@ -33,6 +33,12 @@ app.use((req, res, next) => {
 });
 
 app.use("/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "Server is running 🚀",
+  });
+});
 app.use("/properties", listingRoutes);
 app.use("/bookings", bookingRoutes);
 app.use("/users", userRoutes);
